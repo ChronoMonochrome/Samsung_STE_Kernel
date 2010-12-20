dcaf1f8cdb6cb71ec737cbdac43f6c6f07879d26
Stephen Boyd <sboyd@codeaurora.org>
2010-12-20 14:20:31 -0800

    ARM: Allow machines to override __delay()
    Some machines want to implement their own __delay() routine based
    on fixed rate timers. Expose functionality to set the __delay()
    routine at runtime. This should allow two machines with different
    __delay() routines to happily co-exist within the same kernel
    with minimal overhead.
    
    Russell expressed concern that using a timer based __delay()
    would cause problems when an iomapped device isn't mapped in
    prior to a delay call being made (see
    http://article.gmane.org/gmane.linux.ports.arm.kernel/78543 for
    more info). We can sidestep that issue with this approach since
    the __delay() routine _should_ only be pointed to a timer based
    delay once the timer has been properly mapped. Up until that
    point __delay() and udelay() will use delay_loop() which is
    always safe to call.
    
    This patch is inspired by x86's delay.c
    
    Reviewed-by: Saravana Kannan <skannan@codeaurora.org>
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>
    Change-Id: I269f101b40ba50c2b635dc92d50f6e82bb934b32
    Reviewed-on: http://gerrit.lud.stericsson.com/gerrit/13563
    Tested-by: Mattias WALLIN <mattias.wallin@stericsson.com>
    Reviewed-by: Jonas ABERG <jonas.aberg@stericsson.com>
    
    Conflicts:
    	arch/arm/lib/delay.c

---
+++ arch/arm/lib/delay.c
@@ -0,0 +1,64 @@
+/*
+ *  Originally from linux/arch/arm/lib/delay.S
+ *
+ *  Copyright (C) 1995, 1996 Russell King
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+#include <linux/module.h>
+#include <linux/delay.h>
+
+/*
+ * Oh, if only we had a cycle counter...
+ */
+static void delay_loop(unsigned long loops)
+{
+	asm volatile(
+	"1:	subs %0, %0, #1 \n"
+	"	bhi 1b		\n"
+	: /* No output */
+	: "r" (loops)
+	);
+}
+
+void (*delay_fn)(unsigned long) = delay_loop;
+
+/*
+ * loops = usecs * HZ * loops_per_jiffy / 1000000
+ */
+void __delay(unsigned long loops)
+{
+	delay_fn(loops);
+}
+EXPORT_SYMBOL(__delay);
+
+/*
+ * 0 <= xloops <= 0x7fffff06
+ * loops_per_jiffy <= 0x01ffffff (max. 3355 bogomips)
+ */
+void __const_udelay(unsigned long xloops)
+{
+	unsigned long lpj;
+	unsigned long loops;
+
+	xloops >>= 14;			/* max = 0x01ffffff */
+	lpj = loops_per_jiffy >> 10;	/* max = 0x0001ffff */
+	loops = lpj * xloops;		/* max = 0x00007fff */
+	loops >>= 6;			/* max = 2^32-1 */
+
+	if (likely(loops))
+		__delay(loops);
+}
+EXPORT_SYMBOL(__const_udelay);
+
+/*
+ * usecs  <= 2000
+ * HZ  <= 1000
+ */
+void __udelay(unsigned long usecs)
+{
+	__const_udelay(usecs * ((2199023*HZ)>>11));
+}
+EXPORT_SYMBOL(__udelay);
