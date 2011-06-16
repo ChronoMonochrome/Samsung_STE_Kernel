4a1703c31d0200a3fff721368b4dae828f403ef4
Todd Poynor <toddpoynor@google.com>
2011-06-15 17:44:50 -0700

    ARM: Call idle notifiers
    Change-Id: Id833e61c13baa1783705ac9e9046d1f0cc90c95e
    Acked-by: Nicolas Pitre <nicolas.pitre@linaro.org>
    Signed-off-by: Todd Poynor <toddpoynor@google.com>
    
    Conflicts:
    	arch/arm/kernel/process.c

--- arch/arm/kernel/process.c
+++ arch/arm/kernel/process.c
@@ -194,6 +194,7 @@ void cpu_idle(void)
 	/* endless idle loop with no priority at all */
 	while (1) {
 		tick_nohz_stop_sched_tick(1);
+		idle_notifier_call_chain(IDLE_START);
 		while (!need_resched()) {
 #ifdef CONFIG_HOTPLUG_CPU
 			if (cpu_is_offline(smp_processor_id())) {
@@ -227,6 +228,7 @@ void cpu_idle(void)
 			}
 		}
 		tick_nohz_restart_sched_tick();
+		idle_notifier_call_chain(IDLE_END);
 		preempt_enable_no_resched();
 		schedule();
 		preempt_disable();
