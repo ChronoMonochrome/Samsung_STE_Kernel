48a0a0e98139668679f9e12b0f031cff8128ea7a
Marcin Kaluza <marcin.kaluza@trioptimum.com>
2014-11-01 13:56:11 +0100

    export additional symbols to modules

--- kernel/signal.c
+++ kernel/signal.c
@@ -1274,6 +1274,8 @@ struct sighand_struct *__lock_task_sighand(struct task_struct *tsk,
 	return sighand;
 }
 
+EXPORT_SYMBOL_GPL(__lock_task_sighand);
+
 /*
  * send signal info to all the members of a group
  */
