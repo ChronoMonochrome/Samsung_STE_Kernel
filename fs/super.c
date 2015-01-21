52fa626da1561119659993c37bb57c7be06a291b
Shilin Victor <chrono.monochrome@gmail.com>
2015-01-21 16:17:47 +0400

    fs/super: cwm loop fix
    credits to Team Canjica

--- fs/super.c
+++ fs/super.c
@@ -757,10 +757,13 @@ static void do_emergency_remount(struct work_struct *work)
 		spin_unlock(&sb_lock);
 		down_write(&sb->s_umount);
 		if (sb->s_root && sb->s_bdev && !(sb->s_flags & MS_RDONLY)) {
-			/*
-			 * What lock protects sb->s_flags??
-			 */
-			do_remount_sb(sb, MS_RDONLY, NULL, 1);
+			/* u8500: param.ko needs to update params.blk before rebooting */
+			if (strcmp(sb->s_id, "mmcblk0p1")) {
+				/*
+				 * What lock protects sb->s_flags??
+				 */
+				do_remount_sb(sb, MS_RDONLY, NULL, 1);
+			}
 		}
 		up_write(&sb->s_umount);
 		spin_lock(&sb_lock);
