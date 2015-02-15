c0d23f2398c05056724dfd9a89bdc703031c5e5d
Shilin Victor <chrono.monochrome@gmail.com>
2015-02-15 04:10:19 +0400

    Revert "ARM: 7068/1: process: change from __backtrace to dump_stack in show_regs"
    This reverts commit b380ab4f85d641574d91b12b333848a0731a497c.
    
    Conflicts:
    	arch/arm/kernel/armksyms.c

--- arch/arm/kernel/armksyms.c
+++ arch/arm/kernel/armksyms.c
@@ -49,6 +49,8 @@ extern void __aeabi_ulcmp(void);
 
 extern void fpundefinstr(void);
 
+EXPORT_SYMBOL(__backtrace);
+
 	/* networking */
 EXPORT_SYMBOL(csum_partial);
 EXPORT_SYMBOL(csum_partial_copy_from_user);
--- arch/arm/kernel/process.c
+++ arch/arm/kernel/process.c
@@ -411,7 +411,7 @@ void show_regs(struct pt_regs * regs)
 	printk("\n");
 	printk("Pid: %d, comm: %20s\n", task_pid_nr(current), current->comm);
 	__show_regs(regs);
-	dump_stack();
+	__backtrace();
 }
 
 ATOMIC_NOTIFIER_HEAD(thread_notify_head);
