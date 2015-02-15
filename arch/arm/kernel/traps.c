070eb099066d99fc742bb14252b40c11bab55ff7
Shilin Victor <chrono.monochrome@gmail.com>
2015-02-15 06:06:57 +0400

    arch/arm/kernel/traps.c: samsung changes

--- arch/arm/kernel/traps.c
+++ arch/arm/kernel/traps.c
@@ -25,7 +25,9 @@
 #include <linux/delay.h>
 #include <linux/init.h>
 #include <linux/sched.h>
-
+#ifdef CONFIG_SAMSUNG_LOG_BUF
+#include <linux/mfd/ux500_wdt.h>
+#endif
 #include <asm/atomic.h>
 #include <asm/cacheflush.h>
 #include <asm/system.h>
@@ -253,6 +255,17 @@ static int __die(const char *str, int err, struct thread_info *thread, struct pt
 		dump_instr(KERN_EMERG, regs);
 	}
 
+#ifdef CONFIG_SAMSUNG_LOG_BUF
+#ifdef CONFIG_SAMSUNG_EXTRA_DIE_ACTION
+	{
+		extern void sec_extra_die_actions(const char * str);
+#if 0
+		wdog_disable();
+#endif
+		sec_extra_die_actions(str);
+	}
+#endif
+#endif
 	return ret;
 }
 
@@ -264,11 +277,12 @@ static DEFINE_SPINLOCK(die_lock);
 void die(const char *str, struct pt_regs *regs, int err)
 {
 	struct thread_info *thread = current_thread_info();
+	unsigned long flags;
 	int ret;
 
 	oops_enter();
 
-	spin_lock_irq(&die_lock);
+	spin_lock_irqsave(&die_lock, flags);
 	console_verbose();
 	bust_spinlocks(1);
 	if (!user_mode(regs))
@@ -280,7 +294,7 @@ void die(const char *str, struct pt_regs *regs, int err)
 
 	bust_spinlocks(0);
 	add_taint(TAINT_DIE);
-	spin_unlock_irq(&die_lock);
+	spin_unlock_irqrestore(&die_lock, flags);
 	oops_exit();
 
 	if (in_interrupt())
